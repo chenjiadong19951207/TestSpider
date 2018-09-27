9f7a5369891b90f3d116e42e267e46145fe01f6e
Chenjiadong <c729208005@vip.qq.com>
2018-09-27 23:30:04 +0800

    jobbole

--- TextSpider/items.py
+++ TextSpider/items.py
@@ -4,8 +4,11 @@
 #
 # See documentation in:
 # https://doc.scrapy.org/en/latest/topics/items.html
-
+import datetime
 import scrapy
+from scrapy.loader import ItemLoader
+from scrapy.loader.processors import MapCompose,TakeFirst,Join
+import re
 
 
 class TextspiderItem(scrapy.Item):
@@ -13,15 +16,68 @@ class TextspiderItem(scrapy.Item):
     # name = scrapy.Field()
     pass
 
+def add_jobbole(value):
+    return value+"-jobbole"
+
+def date_convert(value):
+    try:
+        create_date = datetime.datetime.strptime(value, "%Y/%m/%d").date()
+    except Exception as e:
+        create_date = datetime.datetime.now().date()
+    return create_date
+
+
+def remove_comment_tags(value):
+    #去掉tag中提取的评论
+    if '评论' in value:
+        return ''
+    else:
+        return value
+
+def get_nums(value):
+    match_re = re.match(".*?(\d+).*", value)
+    if match_re:
+        nums = int(match_re.group(1))
+    else:
+        nums = 0
+    return nums
+
+
+def return_value(value):
+    return value
+
+
+class ArticleItemLoader(ItemLoader):
+    #自定义itemloader
+    default_output_processor = TakeFirst()
+
 
 class JobBoleArticleItem(scrapy.Item):
-    title = scrapy.Field()
-    create_date = scrapy.Field()
+    title = scrapy.Field(
+        input_processor = MapCompose(add_jobbole)
+    )
+    create_date = scrapy.Field(
+        input_processor=MapCompose(date_convert)
+
+    )
     url = scrapy.Field()
-    front_image_url = scrapy.Field()
+    front_image_url = scrapy.Field(
+        output_processor=MapCompose(return_value)
+    )
     front_image_path = scrapy.Field()
-    praise_nums = scrapy.Field()
-    fav_nums = scrapy.Field()
-    comment_nums = scrapy.Field()
-    tags = scrapy.Field()
+    praise_nums = scrapy.Field(
+
+        input_processor=MapCompose(get_nums)
+    )
+    fav_nums = scrapy.Field(
+
+        input_processor=MapCompose(get_nums)
+    )
+    comment_nums = scrapy.Field(
+        input_processor=MapCompose(get_nums)
+    )
+    tags = scrapy.Field(
+        input_processor=MapCompose(remove_comment_tags),
+        output_processor=Join(',')
+    )
     content = scrapy.Field()
